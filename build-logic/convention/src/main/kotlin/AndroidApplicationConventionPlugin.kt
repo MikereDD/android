db7aaf464adba4ae004f56e1cfad7b41a3089d3a
Mike Redd <323583+MikereDD@users.noreply.github.com>
2026-09-04 19:52:51 -0500

    Set HA-Verity application identity

--- build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidApplicationConventionPlugin.kt
@@ -5,7 +5,8 @@ import org.gradle.api.Project
 import org.gradle.kotlin.dsl.apply
 import org.gradle.kotlin.dsl.configure
 
-private const val APPLICATION_ID = "io.homeassistant.companion.android"
+private const val SOURCE_NAMESPACE = "io.homeassistant.companion.android"
+private const val APPLICATION_ID = "com.typezero.haverity"
 
 /**
  * A convention plugin that applies common configurations to Android application modules.
@@ -39,7 +40,7 @@ class AndroidApplicationConventionPlugin : Plugin<Project> {
             AndroidComposeConventionPlugin().apply(target)
 
             extensions.configure<ApplicationExtension> {
-                namespace = APPLICATION_ID
+                namespace = SOURCE_NAMESPACE
 
                 defaultConfig {
                     applicationId = APPLICATION_ID
